9cf4d41d6cf12ccb84bb5b4e0efa6e4240c5fbfa
luizwanders <wandersomluiz@gmail.com>
2021-04-03 11:30:30 -0300

    refactor: tests paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css'
 import Header from '@/components/shared/Header'
-import Home from '@/components/Home'
+import Home from '@/pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <div>
-            <Header />
-            <main style={{ padding: '0 24px' }}>
-                <Home />
-            </main>
-        </div>
+        <Router>
+            <div>
+                <Header />
+                <main style={{ padding: '0 24px' }}>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,21 +8,16 @@ import { Api } from '@/services/api'
 
 export default function Home() {
     const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
 
     useEffect(async () => {
-        setLoading(true)
         const result = await Api.getCharacters({ limit: 10 })
-        setLoading(false)
         setData(result.data.results)
     }, [])
-
-    const renderLoading = () => <h1>Carregando...</h1>
 
     return (
         <div className="container">
             <div className={style.home}>
-                <div className={style.searcContainer}>
+                <div className={style.searchContainer}>
                     <h1>Busca de personagens</h1>
                     <h2>Nome do personagem</h2>
                     <div className={style.inputContainer}>
@@ -33,7 +28,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            {loading ? renderLoading : <Grid data={data} />}
+            <Grid data={data} />
         </div>
     )
 }
